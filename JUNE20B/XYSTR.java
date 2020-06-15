2f94d38ab7e88edd51476f74edd8de42385cc92d
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-06-16 00:05:47 +0530

    Add files via upload

---
+++ JUNE20B/XYSTR.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class XYSTR
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-->0) {
+
+            String input = sc.next();
+            int count = 0;
+            int start = 0;
+            while (start < input.length()-1) {
+                if (input.substring(start, start+2).equals("xy") || input.substring(start, start+2).equals("yx") ) {
+                    count++;
+                    start+=2;
+                } else {
+                    start +=1;
+                }
+            }
+
+            System.out.println(count);
+        }
+
+    }
+}
